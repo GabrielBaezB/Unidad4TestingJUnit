693416b2e52cb83f83ff9f462d25969545b94030
GabrielBaezB <gabooo@live.com>
2024-08-14 22:35:44 -0400

    Se valida el funcionamientos del test

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,12 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int sum = calculator.add(5, 3);
+        int difference = calculator.subtract(5, 3);
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
     }
 }
