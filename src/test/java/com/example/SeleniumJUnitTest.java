81740e480f919950c020643be3fe185a29b8e287
GabrielBaezB <gabooo@live.com>
2024-08-29 01:13:38 -0400

    se agrega cambiourl para jenkins

--- src/test/java/com/example/SeleniumJUnitTest.java
+++ src/test/java/com/example/SeleniumJUnitTest.java
@@ -29,7 +29,7 @@ public class SeleniumJUnitTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         
-        driver = new ChromeDriver(options);
+        WebDriver driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, Duration.ofSeconds(20)); // Inicializa WebDriverWait con el driver
     }
 
